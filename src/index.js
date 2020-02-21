050337ef8c96d86c304156779fb2a95c5e952369
Siarhei Liavontsyeu <leontevsi@gmail.com>
2020-02-21 16:06:45 +0300

    feat:implement typicalArraysProblem

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
+exports.min = function min(array) {
+    if (typeof array === 'undefined' || array.length === 0) return 0;
 
-exports.min = function min (array) {
-  return 0;
+    let res = array[0];
+
+    for (let i = 1; i < array.length; i++) {
+        if (res > array[i]) {
+            res = array[i];
+        }
+    }
+    return res;
 }
 
-exports.max = function max (array) {
-  return 0;
+
+exports.max = function max(array) {
+    if (typeof array === 'undefined' || array.length === 0) return 0;
+    let res = array[0];
+
+    for (let i = 1; i < array.length; i++) {
+        if (res < array[i]) {
+            res = array[i];
+        }
+    }
+    return res;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    return (typeof array === 'undefined' || array.length === 0) ? 0 : array.reduce((p, n) => (p + n), 0) / array.length;
 }
